bc1d60180369680ab4a661a16f6b6a8aead46d9b
makedream1 <makedream60@gmail.com>
2023-03-22 18:20:13 +0200

    change copy method

--- frontend/src/components/CopyAddress/CopyAddress.tsx
+++ frontend/src/components/CopyAddress/CopyAddress.tsx
@@ -1,24 +1,28 @@
+import useCopyToClipboard from "../../hooks/useCopyToClipboard";
+
 import "./CopyAddress.css";
 
 const CopyAddress = ({ token }: { token: string }) => {
+  const [value, copy] = useCopyToClipboard();
 
   return (
     <div className="CopyAddress">
       <div className="copy-text">
-      <input id="copy-text" type="text" value={token} disabled={true} />
+        <input id="copy-text" type="text" value={token} disabled={true} />
       </div>
       <div
         className="copy-button"
         id="copy-button"
         onClick={() => {
-          let copyText = document.querySelector("#copy-text");
-          // @ts-ignore
-          copyText.select();
-          document.execCommand("copy");
+          copy(token).catch(() => {
+            let copyText = document.querySelector("#copy-text");
+            // @ts-ignore
+            copyText.select();
+            document.execCommand("copy");
+          });
           const x = document.getElementById("copy-button");
           x!.innerHTML = "скопировано";
           setTimeout(() => (x!.innerHTML = "копировать"), 1000);
-          // navigator.clipboard.writeText(token);
         }}
       >
         копировать
