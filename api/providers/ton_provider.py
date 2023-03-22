1295eeea23e0e8943811519467d1189c36b6e27c
makedream1 <makedream60@gmail.com>
2023-03-22 18:00:27 +0200

    update send_mode for api

--- api/providers/ton_provider.py
+++ api/providers/ton_provider.py
@@ -163,7 +163,7 @@ class TON_Provider:
         return balances
 
     async def send_tons(self, wallet, dest: str, amount: int, payload=None,
-                        send_mode: int = 1):
+                        send_mode: int = 1 + 2):
         seqno = await self.get_seqno(wallet.address.to_string(1, 1, 1))
         query = wallet.create_transfer_message(
             dest, amount, seqno, payload=payload, send_mode=send_mode
